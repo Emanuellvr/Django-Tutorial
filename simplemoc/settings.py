f79d8463f0c3c05432c9b51ef09ac07504599147
Emanuel Dias <emanueloliveira@pucminas.br>
2022-07-18 15:20:04 -0300

    Database - Postgres

--- simplemoc/settings.py
+++ simplemoc/settings.py
@@ -76,8 +76,11 @@ WSGI_APPLICATION = 'simplemoc.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'root',
+        'HOST': '10.10.4.3',
     }
 }
 
